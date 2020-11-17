18b4c433d3c7426653df1f040c19805cb623860c
roland-sipos <sipos.roland.cb@gmail.com>
2020-11-17 17:16:34 +0100

    remove udaq test and move to daq-cmake

--- src/DummyFunctionality.cpp
+++ src/DummyFunctionality.cpp
@@ -9,12 +9,22 @@
 
 #include "DummyFunctionality.hpp"
 
+#include <fstream>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
+
 namespace dunedaq {
 namespace daqlib {
 
 void
 DummyFunctionality::do_something()
 {
+
+  auto js = json::parse("{\"happy\": 1}");
+  std::ofstream ofs("foo.txt");
+  ofs << "asd asd \n";
+  ofs << js << '\n';
 
 }
 
