a3ffd9e00b765230e199c7e48b42ca1fb91adc8a
roland-sipos <sipos.roland.cb@gmail.com>
2024-02-19 14:07:29 +0100

    callback demo for datalinkhandlers

---
+++ include/rubberdaq/MockDataLinkHandler.hpp
@@ -0,0 +1,143 @@
+/**
+ * @file MockDataLinkHandler.hpp
+ *
+ * Playground for DLH functionalities
+ *
+ */
+
+#ifndef RUBBERDAQ_INCLUDE_MOCKDATALINKHANDLER_HPP_
+#define RUBBERDAQ_INCLUDE_MOCKDATALINKHANDLER_HPP_
+
+#include "logging/Logging.hpp"
+#include "iomanager/IOManager.hpp"
+
+#include "readoutlibs/models/IterableQueueModel.hpp"
+#include "readoutlibs/utils/ReusableThread.hpp"
+
+#include <iostream>
+#include <functional>
+#include <atomic>
+#include <memory>
+
+namespace dunedaq {
+namespace rubberdaq {
+
+template<typename ROT, typename RECEIVE_TYPE = ROT> // preparing for Alessandro's Capsule idea.
+class MockDataLinkHandler {
+public:
+  MockDataLinkHandler(const MockDataLinkHandler&) = delete;
+  MockDataLinkHandler& operator=(const MockDataLinkHandler&) = delete;
+
+  MockDataLinkHandler() { }
+
+  MockDataLinkHandler(int id, bool callbacks, std::atomic<bool>& run_marker, int numa_node, std::size_t capacity)
+    : m_id(id)
+    , m_callbacks(callbacks)
+    , m_run_marker(run_marker)
+    , m_latency_buffer(capacity, true, numa_node, false, 0)
+    , m_consumer_thread(id)
+    , m_cleanup_thread(id)
+  {
+    std::cout << "New MockDLH with ID[" << m_id << "] is prefilling.\n";
+    m_latency_buffer.force_pagefault();
+
+    if (callbacks) {
+      m_consume_payload = [&, this](ROT&& payload) {
+        m_latency_buffer.write(std::move(payload));
+      };
+      std::cout << "  -> Function pointer/address for consume callback is: " << &m_consume_payload << '\n';
+    } else {
+      //m_consumer_thread.set_work(&MockDataLinkHandler<ROT>::run_consume, this);
+      //m_consumer_thread.set_name("consumer", m_id);
+    }
+
+    m_pop_limit_size = m_pop_limit_pct * capacity;
+    m_cleanup_thread.set_work(&MockDataLinkHandler<ROT>::periodic_cleanups, this);
+    m_cleanup_thread.set_name("cleanup", m_id);
+  }
+
+  ~MockDataLinkHandler() {
+
+  }
+
+  void set_receiver(dunedaq::iomanager::ConnectionId& queue_id) {
+    m_receiver = m_receiver = dunedaq::iomanager::IOManager::get()->get_receiver<ROT>(queue_id);
+  } 
+
+  void start_consumer() {
+    m_consumer_thread.set_work(&MockDataLinkHandler<ROT>::run_consume, this);
+    m_consumer_thread.set_name("consumer", m_id);
+  }
+
+  void run_consume() {
+    auto rawq_timeout_count = 0;
+    auto num_payloads = 0;
+    auto sum_payloads = 0;
+    auto num_payloads_overwritten = 0; 
+    std::chrono::milliseconds timeout_ms = std::chrono::milliseconds(2000);
+
+    while (m_run_marker.load()) {
+      auto opt_payload = m_receiver->try_receive(timeout_ms);
+      if (opt_payload) {
+        ROT& payload = opt_payload.value();
+        if (!m_latency_buffer.write(std::move(payload))) {
+          ++num_payloads_overwritten;
+        }
+        ++num_payloads;
+      } else {
+        ++rawq_timeout_count;
+      } 
+    }
+
+    TLOG() << "Consumer[" << m_id << "]"
+           << " total payloads: " << num_payloads
+           << " timeouts: " << rawq_timeout_count
+           << " overwritten: " << num_payloads_overwritten;
+  } 
+
+  void periodic_cleanups() {
+    while (m_run_marker.load()) {
+      //cleanup_check();
+      if (m_latency_buffer.occupancy() > m_pop_limit_size) {
+        auto size_guess = m_latency_buffer.occupancy();
+        if (size_guess > m_pop_limit_size) {
+          unsigned to_pop = m_pop_size_pct * m_latency_buffer.occupancy();
+          m_latency_buffer.pop(to_pop);
+          m_num_popped += to_pop;
+        }
+        ++m_num_cleanups;
+      } else {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      }
+    }
+  }
+
+  int m_id;
+  bool m_callbacks;
+  std::atomic<uint64_t> m_num_cleanups = 0;
+  std::atomic<uint64_t> m_num_popped = 0;
+  std::atomic<bool>& m_run_marker;
+  dunedaq::readoutlibs::IterableQueueModel<ROT> m_latency_buffer;
+
+  // Optional callback
+  std::function<void(ROT&&)> m_consume_payload;
+
+  // Optional consumer
+  dunedaq::readoutlibs::ReusableThread m_consumer_thread;
+
+  using raw_receiver_ct = iomanager::ReceiverConcept<ROT>;
+  std::shared_ptr<raw_receiver_ct> m_receiver;
+
+  // auto cleanup
+  float m_pop_limit_pct = 0.5f;     // buffer occupancy percentage to issue a pop request
+  float m_pop_size_pct = 0.8f;      // buffer percentage to pop
+  unsigned m_pop_limit_size; // pop_limit_pct * buffer_capacity
+  dunedaq::readoutlibs::ReusableThread m_cleanup_thread;
+
+};
+
+
+} // namespace rubberdaq
+} // namespace dunedaq
+
+#endif // RUBBERDAQ_INCLUDE_MOCKDATALINKHANDLER_HPP_
---
+++ test/apps/test_mock_dlh_app.cxx
@@ -0,0 +1,196 @@
+/**
+ * @file test_mock_dlh_app.cxx Test application for DLH brainstorm
+ *
+ * This is part of the DUNE DAQ Application Framework, copyright 2020.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
+ */
+#include "logging/Logging.hpp"
+
+#include "rubberdaq/MockDataLinkHandler.hpp"
+
+#include "iomanager/IOManager.hpp"
+
+#include "readoutlibs/ReadoutTypes.hpp"
+#include "readoutlibs/models/IterableQueueModel.hpp"
+#include "readoutlibs/utils/RateLimiter.hpp"
+#include "readoutlibs/utils/ReusableThread.hpp"
+#include "fdreadoutlibs/DUNEWIBEthTypeAdapter.hpp"
+
+#include "CLI/App.hpp"
+#include "CLI/Config.hpp"
+#include "CLI/Formatter.hpp"
+
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <future>
+
+using namespace dunedaq;
+using namespace dunedaq::iomanager;
+using namespace dunedaq::rubberdaq;
+using namespace dunedaq::readoutlibs;
+using namespace dunedaq::fdreadoutlibs::types;
+
+namespace dunedaq {
+  DUNE_DAQ_TYPESTRING(dunedaq::fdreadoutlibs::types::DUNEWIBEthTypeAdapter, "WIBEthFrame");
+}
+
+namespace {
+
+  template<typename T>
+  std::map<int, std::unique_ptr<std::function<void(T&&)>>> CallbackMap = {};
+
+  int num_streams = 40;
+  float prod_rate = 30.5;
+  int lb_numa_node = 0;
+  std::size_t lb_capacity = 124992; // LB capacity
+  int run_for_secs = 60;
+  bool consumer_thread_mode = false;
+  bool consumer_callback_mode = false;
+
+}
+
+int
+main(int argc, char** argv)
+{
+  CLI::App app{"rubberdaq_mock_dlh"};
+  app.add_option("-n", num_streams, "Number of data streams in the test.");
+  app.add_option("--rate", prod_rate, "Rate of data producers. [kHz]");
+  app.add_option("--lb_numa_node", lb_numa_node, "NUMA node for LBs to allocate on.");
+  app.add_option("-c", lb_capacity, "Capacity/size of latency buffer.");
+  app.add_option("--run_secs", run_for_secs, "How many seconds the test should run.");
+  app.add_flag("--ct", consumer_thread_mode, "Consumer threads mode.");
+  app.add_flag("--cb", consumer_callback_mode, "Consume callback mode.");
+  CLI11_PARSE(app, argc, argv);
+
+  if (!(consumer_thread_mode || consumer_callback_mode)) {
+    std::cout << "Neither threaded or callback consumers are requested. Won't run the test.\n";
+    return 0;
+  } else if (consumer_thread_mode && consumer_callback_mode) {
+    std::cout << "Both thread and callback mode requested. Won't run the test.\n";
+    return 0;
+  } else if (consumer_thread_mode) {
+    std::cout << "Test with consumer threads...\n";
+  } else if (consumer_callback_mode) {
+    std::cout << "Test with consumer callback...\n";
+  }
+
+  // If consumers, set up IOManager
+  if (consumer_thread_mode) {
+    setenv("DUNEDAQ_SESSION", "IOManager_t", 0);
+    dunedaq::iomanager::Queues_t queues;
+    for (unsigned i=0; i<num_streams; ++i) {
+      auto qidstr = "queue-" + std::to_string(i);
+      ConnectionId queue_id = ConnectionId{ qidstr, "WIBEthFrame" };
+      queues.emplace_back(QueueConfig{ queue_id, QueueType::kFollySPSCQueue, 10000 });
+    }
+    dunedaq::iomanager::Connections_t connections;
+    IOManager::get()->configure(queues, connections, false, 1000ms);
+  }
+
+  // Run marker
+  std::atomic<bool> marker{ true };
+
+  // Create Mock DataLinkHandlers
+  std::map<int, std::unique_ptr<MockDataLinkHandler<DUNEWIBEthTypeAdapter>>> dlh_map;
+  for (unsigned i=0; i<num_streams; ++i) {
+    // Create ith MockDLH
+    if (consumer_callback_mode) {
+      dlh_map[i] = std::make_unique<MockDataLinkHandler<DUNEWIBEthTypeAdapter>>(i, true, marker, lb_numa_node, lb_capacity);
+      // Register consume callback to a map with id i
+      CallbackMap<DUNEWIBEthTypeAdapter>[i] = std::make_unique<std::function<void(DUNEWIBEthTypeAdapter&&)>>(dlh_map[i]->m_consume_payload);
+      std::cout << " Created CallbackMap's function pointer/address is: " << &CallbackMap<DUNEWIBEthTypeAdapter>[i] << '\n';
+    } else if (consumer_thread_mode) {
+      dlh_map[i] = std::make_unique<MockDataLinkHandler<DUNEWIBEthTypeAdapter>>(i, false, marker, lb_numa_node, lb_capacity);
+      auto qidstr = "queue-" + std::to_string(i);
+      ConnectionId queue_id = ConnectionId{ qidstr, "WIBEthFrame" };
+      dlh_map[i]->set_receiver(queue_id);
+      dlh_map[i]->start_consumer();
+    }
+
+  }
+
+  // RateLimiter
+  std::cout << "Creating ratelimiter with " << prod_rate << "[kHz]...\n";
+  RateLimiter rl(prod_rate);
+
+  // Create data producer threads
+  std::map<int, std::thread> producer_map;
+  for (unsigned i=0; i<num_streams; ++i) {
+    if (consumer_callback_mode) { // go through Callbacks
+      auto* cbref = CallbackMap<DUNEWIBEthTypeAdapter>[i].get();
+      producer_map[i] = std::thread([&, cbref]() {
+        uint64_t tot_produced = 0;
+        uint64_t ts = 0; // NOLINT(build/unsigned)
+        while (marker.load()) {
+          DUNEWIBEthTypeAdapter pl;
+          pl.set_first_timestamp(ts);
+          (*cbref)(std::move(pl));
+          ts += 32;
+          ++tot_produced;
+          rl.limit();
+        }
+        TLOG() << "Total produced: " << tot_produced;
+      });
+
+    } else if (consumer_thread_mode) { // go through IOManager
+      auto qidstr = "queue-" + std::to_string(i);
+      ConnectionId queue_id = ConnectionId{ qidstr, "WIBEthFrame" };
+      auto sender = dunedaq::iomanager::IOManager::get()->get_sender<DUNEWIBEthTypeAdapter>(queue_id);
+      producer_map[i] = std::thread([&, sender]() { 
+        uint64_t tot_produced = 0;
+        uint64_t ts = 0; // NOLINT(build/unsigned)
+        uint64_t dropped = 0;
+        while (marker.load()) {
+          DUNEWIBEthTypeAdapter pl;
+          pl.set_first_timestamp(ts);
+          if (!sender->try_send(std::move(pl), iomanager::Sender::s_no_block)) {
+            ++dropped;
+          }
+          ts += 32;
+          ++tot_produced;
+          rl.limit();
+        }
+        TLOG() << "Total produced: " << tot_produced << " total dropped: " << dropped;
+      });
+    }
+
+    std::cout << "Producer [" << i << "] spawned.\n";
+  }
+
+  // Name producer threads
+  for (auto& [id, prod] : producer_map) {
+    char tname[16];
+    snprintf(tname, 16, "%s-%d", "producer", id); // NOLINT
+    auto handle = prod.native_handle();
+    pthread_setname_np(handle, tname);
+  }
+
+  // Killswitch that flips the run marker
+  auto killswitch = std::thread([&]() {
+    std::cout << "Application will terminate in " << run_for_secs << " seconds...\n";
+    std::this_thread::sleep_for(std::chrono::seconds(run_for_secs));
+    marker.store(false);
+  });
+
+  // Join every threads
+  std::cout << "Flipping killswitch that will start the countdown...\n";
+  if (killswitch.joinable()) {
+    killswitch.join();
+  }
+  
+  // Join producers
+  for (auto& [id, prod] : producer_map) {
+    if (prod.joinable()) {
+      prod.join();
+      std::cout << "Producer [" << id << "] joined.\n";
+    }
+    std::cout << "MockDLH[" << id << "] total cleanups: " << dlh_map[id]->m_num_cleanups 
+      << " total elements popped: " << dlh_map[id]->m_num_popped << '\n';
+  }
+
+  // Exit
+  std::cout << "Exiting.\n";
+  return 0;
+}
